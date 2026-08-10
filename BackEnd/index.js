47d11ab139d333984b4c2573abd32fee6f49eecb
Urooj-Zafar <uroojzafar116@gmail.com>
2026-08-10 18:03:41 +0500

    updation
    fixing

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -11,24 +11,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect Database
+
 dataBase();
 
-// Static Files
+
 app.use("/uploads", express.static(path.join(path.resolve(), "uploads")));
 
-// Load routes AFTER dotenv
 const UsersRoutes = (await import("./routes/Users.js")).default;
 const BlogsRoutes = (await import("./routes/Blogs.js")).default;
 const CategoriesRoutes = (await import("./routes/Categories.js")).default;
 
-// Routes
 app.use("/users", UsersRoutes);
 app.use("/blogs", BlogsRoutes);
 app.use("/categories", CategoriesRoutes);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;

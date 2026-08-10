9873de784635c7eefc5caa78026c2cc23fdb714c
Urooj-Zafar <uroojzafar116@gmail.com>
2026-08-10 18:25:45 +0500

    fix
    fix

--- BackEnd/authMiddleware.js
+++ BackEnd/authMiddleware.js
@@ -5,7 +5,7 @@ export default function auth(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // attach user info to req
+    req.user = decoded; 
     next();
   } catch (err) {
     return res.status(401).json({ status: false, message: "Invalid token" });

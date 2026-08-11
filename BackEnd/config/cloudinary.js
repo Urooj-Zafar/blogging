987401799b208ca120dc112ec7832e5734b4f89a
Urooj-Zafar <uroojzafar116@gmail.com>
2026-08-11 16:09:53 +0500

    secret
    secret

--- BackEnd/config/cloudinary.js
+++ BackEnd/config/cloudinary.js
@@ -4,6 +4,13 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
+console.log("Cloudinary config:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key_exists: !!process.env.CLOUDINARY_API_KEY,
+  api_secret_exists: !!process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
